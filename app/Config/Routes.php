66063ddd91b79a18f292b19b02332865e002e580
10220046_FakhriAkmalFadillah <10220046@bsi.ac.id>
2023-10-09 14:11:18 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/latihan/(:num)/(:num)', 'Latihan1::penjumlahan/$1/$2');
